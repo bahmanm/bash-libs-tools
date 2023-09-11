0b0bd2713c1da4f01c9c422038f28a71c7a294cd
Bahman Movaqar <bahman@bahmanm.com>
2023-09-11 05:32:32 -0700

    Add helper functions to deal w/ PATH
    - _.all_in_path
    _ _.next_in_path
    _ _.array_contains
    
    Additionally delete obsolete functions

--- main.sh
+++ main.sh
@@ -62,405 +62,100 @@ EOF
     | xargs -r kill -"$sig"
 }
 
-################################################################################
-### package git
-################################################################################
+####################################################################################################
+# _.array_contains VALUE *ELEMENT
+#
+# Checks if VALUE is found in ELEMENTs.
+#
+# Example
+#
+#   A=( foo bar baz )
+#   $ _array_contains bar "$A[*]" && echo "It should return 0"
+#   > It should return 0
+#   $ _array_contains lorem "$A[*]" || echo "It should return 1"
+#   > It should return 1
+#
+# @param VALUE Value to check
+# @param *ELEMENT Array elements
+# @return 0 if found, 1 otherwise.
+# @output N/A
+####################################################################################################
 
-###############
-# git.update_repo(repo, stash_uncommitted=true)
-# output: n/a
-# returns: 0 if successful (or no remote tracking ref)
-# fetches all the remotes, stashes any uncommitted index work, and resets
-# the HEAD to the tip of the current tracking ref.
-###############
-function git.update_repo {
-  local repo tracking_ref stash_uncommitted
+function _.array_contains {
+  local value="$1"
 
-  repo="$1"
-  pushd "$repo"
-  tracking_ref=$(git._tracking_ref "$repo") || return 0
-  stash_uncommitted=[[ "${2:-true}" == 'true' ]]
-  if git.has_uncommitted "$repo" && $stash_uncommitted; then
-    git stash push -q
-  fi
-  git fetch -q --all > /dev/null \
-    && git reset -q --hard "$tracking_ref" > /dev/null || exit 1
-  popd
+  local element
+  for element in ${@:2}; do
+    if [[ "$element" == "$value" ]]; then
+      return 0
+    fi
+  done
+  return 1
 }
 
-###############
-# git._tracking_ref(repo)
-# output: tracking branch or empty
-# returns: 0 if branch tracked, 1 otherwise
-###############
-function git._tracking_ref {
-  local repo result
+####################################################################################################
+# _.all_in_path CMD
+#
+# Searches PATH for all entries containg CMD.
+#
+# Example:
+#  $ echo $(_.all_in_path make)
+#  > /usr/bin/make /bin/make
+#  $ echo $(_.all_in_path non-existing-command)
+#  >
+#
+# @param CMD Name of the command
+# @output All matching entries in PATH as absolute paths to CMD
+# @return 0
+####################################################################################################
 
-  repo="$1"
-  pushd "$repo"
-  result=$(git rev-parse --abbrev-ref --symbolic-full-name @{u} 2>/dev/null)
-  if (( $? == 0)); then
-    echo "$result"
-  else
-    return 1
-  fi
-}
-
-###############
-# git.has_uncommitted(repo)
-# returns: 0 if `repo` has uncommitted changes, non-zero otherwise.
-#          exits w/ 1 in case of failure
-# output: n/a
-###############
-function git.has_uncommitted {
-  local repo result
-
-  repo="$1"
-  pushd "$repo"
-  result=$(git status --short | wc -l) || exit 1
-  popd
-  if (( $result > 0 )); then
-    return 0
-  else
-    return 1
-  fi
-}
-
-################################################################################
-### package ws
-###
-# sample ws (json)
-# {
-#   "dirs": {
-#     "/path/to/dir/1": {
-#       "includes": "\\.java$|\\.scala$",
-#       "excludes": ".*Fixtures\\.java$|.*Resource\\.scala"
-#     }
-#   },
-#   "tagfile": "/path/to/some/arbitrary/dir/tags",
-#   "root": "/path/to/some/arbitrary/dir"
-# }
-################################################################################
-
-WS_DB_HOME="$HOME/.config/bahman/ws"
-
-###############
-# ws._get_tagfile(ws)
-# returns: exits w/ 1 if ws does not exists, 0 otherwise
-# outputs: tagfile path
-###############
-function ws._get_tagfile {
-  local ws result
-
-  ws=$(ws._abspath "$1")
-  result=$(jq -er '.tagfile' < "$ws") || exit 1
-  echo "$result"
-}
-
-###############
-# ws._get_root(ws)
-# returns: exits w/ 1 if ws does not exists, 0 otherwise
-# outputs: root path
-###############
-function ws._get_root {
-  local ws result
-
-  ws=$(ws._abspath "$1")
-  result=$(jq -er '.root' < "$ws") || exit 1
-  echo "$result"
-}
-
-###############
-# ws._get_dirs(ws, as_json=false)
-# returns: exits w/ 1 if ws does not exists, 0 otherwise
-# outputs: dirs
-###############
-function ws._get_dirs {
-  local ws as_json result
-
-  ws=$(ws._abspath "$1") && ws.verify_exists "$ws"
-  as_json="${2:-false}"
-  if [[ "$as_json" == 'false' ]]; then
-    result=$(jq -er '.dirs | keys[]' < "$ws") || exit 1
-  else
-    result=$(jq -er '.dirs | keys' < "$ws") || exit 1
-  fi
-  echo "$result"
-}
-
-###############
-# ws._get_dir(ws, dir)
-# returns: exits w/ 1 if ws/dir does not exists, 0 otherwise
-# outputs: dir as json
-###############
-function ws._get_dir {
-  local ws dir result
-
-  ws=$(ws._abspath "$1") && ws.verify_exists "$ws"
-  dir="$2"
-  result=$(jq -er ".dirs.\"$dir\"" < "$ws") || exit 1
-  echo "$result"
-}
-
-###############
-# ws.dir._get_includes(dir)
-# returns: exits w/ 1 if ws does not exists, 0 otherwise
-# outputs: includes
-###############
-function ws.dir._get_includes {
-  local dir result
-  dir="$1"
-  result=$(echo "$1" | jq -er '.includes') || exit 1
-  echo "$result"
-}
-
-###############
-# ws.dir._get_excludes(dir)
-# returns: exits w/ 1 if ws does not exists, 0 otherwise
-# outputs: excludes
-###############
-function ws.dir._get_excludes {
-  local dir result
-  dir="$1"
-  result=$(echo "$1" | jq -er '.excludes') || exit 1
-  echo "$result"
-}
-
-###############
-# ws._abspath(ws)
-# output: full path to `ws`
-# returns: 0
-###############
-function ws._abspath {
-  if [[ "$1" == $WS_DB_HOME/* ]]; then
-    echo "$1"
-  else
-    echo "$WS_DB_HOME/$1"
-  fi
-}
-
-###############
-# ws._delete(ws)
-# returns: 0
-###############
-function ws.delete {
-  local ws=$(ws._abspath "$1")
-  rm -f "$ws"
-}
-
-###############
-# ws._init(ws, root, tagfile)
-# output: n/a
-# returns: 0
-###############
-function ws._init {
-  [[ '-h' == "$1" ]] && echo 'ws._init ws root=cwd tagfile=TAGS' && return
-
-  local ws=$(ws._abspath "$1")
-  local root=$(readlink -f "${2:-$(pwd)}")
-  local tagfile="$root/${3:-TAGS}"
-  if [[ ! -d "$root" ]]; then
-    mkdir -p $"root"
-  fi
-  if [[ ! -f "$ws" ]]; then
-    echo '{}' \
-      | jq -r ".id = \"$1\"" \
-      | jq -r ".dirs = {}" \
-      | jq -r ".tagfile = \"$tagfile\"" \
-      | jq -r ".root = \"$root\"" > "$ws"
-  fi
-}
-
-###############
-# ws.verify_exists(ws)
-# output: error message if does not exist
-# returns: 0 if exists, exits w/ 1 otherwise
-###############
-function ws.verify_exists {
-  local ws=$(ws._abspath "$1")
-  if [[ ! -f "$ws" ]]; then
-    echo "missing workspace - use `ws._init`" >&2
-    exit 1
-  fi
-}
-
-###############
-# ws.dirs._add(ws, dir, includes='.*', excludes='^$')
-# output: n/a
-# returns: 0
-###############
-function ws.dirs._add {
-  local ws includes excludes dir tmp_ws
-
-  ws=$(ws._abspath "$1") &&  ws.verify_exists "$1"
-  includes="${3:-.*}"
-  excludes="${4:-^$}"
-  dir=$(readlink -f "$2")
-  tmp_ws=$(mktemp)
-  { jq -r ".dirs.\"$dir\" = {}" < "$ws" \
-      | jq -r ".dirs.\"$dir\".includes = \"$includes\"" \
-      | jq -r ".dirs.\"$dir\".excludes = \"$excludes\"" > "$tmp_ws"; } \
-    && cp -f "$ws" "$ws.backup" \
-    && mv "$tmp_ws" "$ws"
-}
-
-###############
-# ws.dirs._delete(ws, dir)
-# output: n/a
-# returns: 0
-###############
-function ws.dirs._delete {
-  [[ '-h' == "$1" ]] && echo 'ws.dirs._delete ws dir' && return
-
-  local ws ws_root dir
-  ws=$(ws._abspath "$1") &&  ws.verify_exists "$1"
-  dir="$2"
-  tmp_ws=$(mktemp)
-  jq -er "del(.dirs.\"$dir\")" < "$ws" > "$tmp_ws" \
-    && cp -f "$ws" "$ws.backup" \
-    && mv "$tmp_ws" "$ws" \
-    && rm -rf "$dir"
-}
-
-###############
-# ws.generate_tags(ws)
-# (re-)generate the ws tags file
-# returns: 0
-# outputs: n/a
-###############
-function ws.generate_tags {
-  local ws tagfile dirs dir_data includes excludes
-
-  ws="$1"
-  dirs=$(ws._get_dirs "$ws")
-  tagfile=$(ws._get_tagfile "$ws")
-  if [[ -f "$tagfile" ]]; then
-    mv "$tagfile" "$tagfile.backup"
-  fi
-  for dir in $dirs; do
-    dir_data=$(ws._get_dir "$ws" "$dir")
-    includes=$(ws.dir._get_includes "$dir_data") || exit 1
-    excludes=$(ws.dir._get_excludes "$dir_data") || exit 1
-    tag.generate "$dir" "$tagfile" "$includes" "$excludes"
+function _.all_in_path {
+  local cmd="$1"
+  local paths=( "${PATH//:/ }" )
+  local matching_abspaths=()
+  for path in ${paths[@]}; do
+    local abspath="$path/$cmd"
+    if [[ -x "$abspath" ]] && ! _.array_contains "$abspath" "${matching_abspaths[*]}"; then
+      matching_abspaths+=( "$abspath" )
+      echo $abspath
+    fi
   done
 }
 
-###############
-# ws.update_vcs(ws, [dir]?)
-###############
-function ws.update_vcs {
-  local ws dirs
+####################################################################################################
+# _.next_in_path CMD DIR
+#
+# Finds the next entry in PATh which contains CMD.
+#
+# Example:
+#
+#  $ echo $(_.next_in_path make /usr/bin)
+#  > /bin/make
+#  $ echo $(_.next_in_path make /bin)
+#  >
+#
+# @param CMD Name of the command
+# @param DIR Absolute path to the directory containing CMD
+# @output Absolute path to the next occurence of `command` in PATH
+# @return 1 if no other occurence of `command` is found in PATH, 0 otherwise.
+####################################################################################################
 
-  ws="$1"
-  if (( $# > 1 )); then
-    dirs="${@:2}"
-  else
-    dirs=$(ws._get_dirs "$ws") || exit 1
-  fi
-  for dir in $dirs; do
-    git.update_repo "$dir" || exit 1
+function _.next_in_path {
+  local cmd="$1"
+  local current_dir="$2"
+  local current_abspath="$2/$1"
+  local all=( $(_.all_in_path $cmd) )
+  local found_current_cmd=false
+
+  for entry in ${all[@]}; do
+    if [[ "$entry" == "$current_abspath" ]]; then
+      found_current_cmd=true
+    elif [[ "$found_current_cmd" == true ]]; then
+      echo $entry
+      return 0
+    fi
   done
-}
-
-###############
-# ws.create(ws, branch_name, git_repos)
-# adds checked out working trees for each `git_repo` using the branch name
-# `branch_name`.
-# output: none
-# returns: 0
-###############
-function ws.create {
-  [[ '-h' == "$1" ]] && echo 'ws.create ws ws_root branch git_repos[]' && return
-
-  local ws ws_root branch_name
-  local -a git_repos
-
-  ws="$1"
-  ws_root="$2"
-  ws._init "$ws" "$ws_root"
-  branch_name="$3"
-  read -r -a git_repos <<< "${@:4}"
-  for repo in "${git_repos[@]}"; do
-    ws.add_repo "$ws" "$branch_name" "$repo"
-  done
-}
-
-###############
-# ws.add_repo(ws, branch_name, git_repo)
-# adds checked out working tree of `git_repo` using the branch name
-# `branch_name`.
-# output: none
-# returns: 0
-###############
-function ws.add_repo {
-  [[ '-h' == "$1" ]] && echo 'ws.add_repo ws branch git_repo' && return
-
-  local ws branch_name git_repo
-  local ws_root worktree_path git_cmd git_dir
-
-  ws="$1"
-  ws_root=$(ws._get_root "$ws")
-  branch_name="$2"
-  git_repo="$3"
-  git_dir="${git_repo}/.git"
-  worktree_path="${ws_root}/$(basename $git_repo)"
-  git_cmd="git --git-dir=$git_dir"
-  $git_cmd fetch -q \
-    && $git_cmd worktree add -q -b "$branch_name" "$worktree_path" origin/master \
-    && ws.dirs._add "$ws" "$worktree_path"
-}
-
-###############
-# ws.delete_repo(ws, worktree_name)
-function ws.delete_repo {
-  [[ '-h' == "$1" ]] && echo 'ws.delete_repo ws worktree_name' && return
-
-  local ws worktree_name
-  local ws_root worktree_path git_dir
-
-  ws="$1"
-  ws_root=$(ws._get_root "$ws")
-  worktree_name="$2"
-  worktree_path="${ws_root}/${worktree_name}"
-  git_dir=$(git --git-dir="${worktree_path}/.git" rev-parse --git-common-dir) \
-    || return 1
-  git --git-dir="$git_dir" worktree remove -f "$worktree_path" \
-    && ws.dirs._delete "$ws" "$worktree_path"
-}
-
-################################################################################
-### package tag
-################################################################################
-
-###############
-# tag.generate(dir, tag_file='TAGS,
-#              include_patterns='*', exclude_patterns='',
-#              is_tracked_only=true)
-# note: includes/excludes are pcre patterns, eg '\.java$|\.groovy$'.
-# returns: n/a
-# output: 0 if all successfull
-###############
-function tag.generate {
-  local dir tag_file includes excludes is_tracked_only files_cmd perl_cmd
-
-  dir="$1"
-  tag_file="${2:-$dir/TAGS}"
-  includes="${3:-.*}"
-  excludes="${4:-^\$}"
-  is_tracked_only="${5:-true}"
-  #
-  pushd "$dir"
-  if [[ "$is_tracked_only" == 'true' ]]; then
-    files_cmd="git ls-tree -r --name-only @"
-  else
-    files_cmd="find $dir -type f -name '*'"
-  fi
-  perl_cmd="print \"$dir/\$_\" if /($includes)/ && !/($excludes)/"
-  #
-  $files_cmd \
-    | perl -nE"$perl_cmd" \
-    | ctags -a -L - -e -u --extras=-fqrs --fields=+aNFE -f "$tag_file"
-  popd
+  return 1
 }
 
 set +o nounset
